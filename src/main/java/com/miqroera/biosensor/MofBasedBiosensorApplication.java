6fc13c6b733ce30634f6bdd65a3e179685468330
董景祥 <dongjingxiang@cl.inside>
2026-03-26 14:30:52 +0800

    init

---
+++ src/main/java/com/miqroera/biosensor/MofBasedBiosensorApplication.java
@@ -0,0 +1,13 @@
+package com.miqroera.biosensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MofBasedBiosensorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MofBasedBiosensorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miqroera/biosensor/MofBasedBiosensorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miqroera.biosensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MofBasedBiosensorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
